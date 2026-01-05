676f56b2dc9fda94e284f570185d3859fb676d55
Prashansa012 <goswamiprashansa@gmail.com>
2026-01-05 17:04:15 +0530

    extra file delete

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
- # try:
-    #     print(largest_number([1,6.7]))
-    # except TypeError as t:
-    #     print(f"An error occurred: {t}")
-    #     # print("input must be a list")
-    # except IndexError:
-    #     print("list must have atleast 1 element")
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
